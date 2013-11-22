0562b1d4460cb9a49744d9a046500f9c5ed5b317
Danwolf1 <daniel.wiazowski@hotmail.com>
2013-11-22 15:51:52 +0100

    deskopt - aktualizacja

--- desktop/duta_deskopt/MainWindow.xaml.cs
+++ desktop/duta_deskopt/MainWindow.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -20,21 +22,33 @@ namespace duta_deskopt
     /// </summary>
     public partial class MainWindow : Window
     {
+        private duta_deskopt.MessangerBox box;
+        private List<GetContactListResult> contactList;
+        private DutaServices ds;
+        private LoginResult me;
+
         public MainWindow()
         {
             InitializeComponent();
-            generateContactList();
+
+            ds = new DutaServices();
+            me = ds.Login("user_c", "pass");
+            myState.SelectedIndex = 0;
+            contactList = ds.getContactList();
+            generateContactList(contactList);
+            
+            
         }
 
-        private void generateContactList() 
+        private void generateContactList(List<GetContactListResult> contactList) 
         {
-
-
-            contacts.Items.Add(generateContact());
-
+            foreach (GetContactListResult contact in contactList)
+            {
+                contacts.Items.Add(generateContact(contact));
+            }          
         }
 
-        private ListBoxItem generateContact()
+        private ListBoxItem generateContact(GetContactListResult contact)
         {
             ListBoxItem item = new ListBoxItem();
             Grid grid = new Grid();
@@ -44,19 +58,22 @@ namespace duta_deskopt
             grid.ColumnDefinitions.Add(new ColumnDefinition { Width = new GridLength(1, GridUnitType.Star) });
             grid.ColumnDefinitions.Add(new ColumnDefinition { Width = new GridLength(1, GridUnitType.Star) });
 
+            item.Selected += new RoutedEventHandler(item_Selected);
+            item.Name = contact.nickname;
+
             Label state = new Label();
-            state.Content = "status";
+            state.Content = contact.status;
             Grid.SetColumn(state, 0);
             Grid.SetRowSpan(state, 2);
             grid.Children.Add(state);
 
             Label user = new Label();
-            user.Content = "uzytkownik";
+            user.Content = contact.nickname;
             Grid.SetColumn(user, 1);
             grid.Children.Add(user);
 
             Label desc = new Label();
-            desc.Content = "opis";
+            desc.Content = contact.description;
             Grid.SetColumn(desc, 1);
             Grid.SetRow(desc, 1);
             
@@ -64,6 +81,29 @@ namespace duta_deskopt
             item.Content = grid;
 
             return item;
+        }
+
+        private void item_Selected(object sender, RoutedEventArgs e)
+        {
+            ListBoxItem item = (ListBoxItem)sender;
+            if (box == null)
+            {
+                box = new MessangerBox(item.Name.ToString(), contactList,ds,me.user_id);
+                box.Show();
+            }
+            else {
+                box.newTab(item.Name.ToString());
+               
+            }
+            
+            
+        }
+
+        public void descChange_Click(object sender, RoutedEventArgs e)
+        {
+            ComboBoxItem state = (ComboBoxItem)myState.SelectedItem;
+            int idState = Convert.ToInt32((string)state.Tag);
+            ds.setStatus(idState, description.Text);
         }
     }   
 }
